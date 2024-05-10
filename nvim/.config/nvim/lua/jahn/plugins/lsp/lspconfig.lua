24c0dbc864eda06d8f3b0ab9e8b949d44a010e0c
joaooliveira-proof <joao.oliveira@proof.com.br>
2024-05-10 17:50:33 -0300

    feat: add signs to lspconfig

--- nvim/.config/nvim/lua/jahn/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/jahn/plugins/lsp/lspconfig.lua
@@ -1,23 +1,28 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    { "folke/neodev.nvim", opts = {} },
-  },
-  config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{ "folke/neodev.nvim", opts = {} },
+	},
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-local lspconfig = require('lspconfig')
-local mason_lspconfig = require("mason-lspconfig")
+		local lspconfig = require("lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
 
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-mason_lspconfig.setup_handlers({
-    function(lsp)
-  lspconfig[lsp].setup ({
-    capabilities = capabilities,
-  })
-    end
-})
-  end
+		-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+		mason_lspconfig.setup_handlers({
+			function(lsp)
+				lspconfig[lsp].setup({
+					capabilities = capabilities,
+				})
+			end,
+		})
+		local signs = { Error = "┃ ", Warn = "┃ ", Hint = "┃ ", Info = "┃ " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+		end
+	end,
 }
