48319439c12c4ec76d4b228e159372eea81e5f30
joaooliveira-proof <joao.oliveira@proof.com.br>
2024-05-30 09:47:37 -0300

    feat: add gif diff binding

--- nvim/.config/nvim/lua/jahn/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/jahn/plugins/vim-fugitive.lua
@@ -3,5 +3,6 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git status" })
+		vim.keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<CR>", { desc = "Git diff" })
 	end,
 }
