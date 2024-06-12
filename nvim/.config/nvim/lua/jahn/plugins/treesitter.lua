5a5483eb94048e436c3964ec477bbb78a500b078
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-06-12 08:47:34 -0300

    feat: ensure go is installed in treesitter

--- nvim/.config/nvim/lua/jahn/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/jahn/plugins/treesitter.lua
@@ -1,23 +1,32 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-        "windwp/nvim-ts-autotag"
-    },
-    config = function()
-        local treesitter = require("nvim-treesitter.configs")
-        treesitter.setup(        {
-            highlight = {
-                enable = true,
-            },
-            indent = { enable = true},
-            autotag = {
-                enable = true,
-            },
-            ensure_installed = { "lua", "python", "typescript", "html", "css", "svelte", "dockerfile", "markdown", "markdown_inline"}
-        }
-        )
-    end
-
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
+		treesitter.setup({
+			highlight = {
+				enable = true,
+			},
+			indent = { enable = true },
+			autotag = {
+				enable = true,
+			},
+			ensure_installed = {
+				"lua",
+				"python",
+				"typescript",
+				"html",
+				"css",
+				"svelte",
+				"dockerfile",
+				"markdown",
+				"markdown_inline",
+				"go",
+			},
+		})
+	end,
 }
