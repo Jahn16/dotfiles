c11ef27e64ec369299425b08ab03387647e21e02
joaooliveira-proof <joao.oliveira@proof.com.br>
2024-08-06 14:02:15 -0300

    feat: use ruff as formatter

--- nvim/.config/nvim/lua/jahn/plugins/conform.lua
+++ nvim/.config/nvim/lua/jahn/plugins/conform.lua
@@ -17,7 +17,7 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				python = { "isort", "black" },
+				python = { "ruff_fix", "ruff_format" },
 				javascript = { "prettier" },
 				typescript = { "prettier" },
 				html = { "prettier" },
