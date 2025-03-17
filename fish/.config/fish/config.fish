d1504d5df6c614cdfe8897de27735a040b10eedd
Jahn16 <jpdeoliveiraferreira@gmail.com>
2025-03-17 20:32:17 -0300

    chore: add vim abbr

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+abbr -a vim nvim
+
 alias ls='eza --icons --color=auto'
 alias la='eza -a'
 alias ll='eza -alF'
